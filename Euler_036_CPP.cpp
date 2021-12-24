9f6fe82c1472edb8d58cf2d0ce7a4eec73654acf
Ariel Tynan <tynan.a@northeastern.edu>
2021-12-23 19:48:44 -0700

    Add project files.

---
+++ Euler_036_CPP.cpp
@@ -0,0 +1,9 @@
+// Euler_036_CPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
