5e443ac5171a5507833178fe18b7129ed2fcf60a
Ariel Tynan <tynan.a@northeastern.edu>
2021-12-23 21:35:34 -0700

    Complete solve! Current runtime, <7s

--- Euler_036_CPP.cpp
+++ Euler_036_CPP.cpp
@@ -1,5 +1,6 @@
 // Euler_036_CPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+// Current runtime sub-7 seconds
 
 #include <iostream>
 #include <vector>
@@ -7,36 +8,26 @@
 
 using namespace std;
 
-int decToBinary(int bin) //function taken from GeekstoGeeks and modified
+vector<int> decToBinary(int bin) //function taken from GeekstoGeeks and heavily modified
 {
     // array to store binary number
-    int binaryNum[32];
+    vector<int> binaryNum;
 
     // counter for binary array
     int i = 0;
     while (bin > 0) {
 
         // storing remainder in binary array
-        binaryNum[i] = bin % 2;
+        binaryNum.push_back(bin % 2);
         bin = bin / 2;
         i++;
     }
-
-    // printing binary array in reverse order
-    int biVal = 0;
-    for (int j = 0; j < i; j++)
-        biVal = 10 * biVal + (binaryNum[j] + 1); //binary of 1,2 instead of 0,1 because zeroes not showing up
-    //cout << biVal << endl;
-    return biVal;
+    return binaryNum;
 }
 
-bool palCheck(int num, vector<int> v)
+bool palCheck(vector<int> v)
 {
-    while (num != 0) //separates ints into digits
-    {
-        v.push_back((num % 10)*1.0 + .5);
-        num /= 10;
-    }
+
     bool palin = true;
     for (int j = 0; j < v.size(); j++)
     {
@@ -52,14 +43,19 @@ int main()
     int total = 0;
     for (int i = 0; i < n/2; i++) //checks all numbers after 1, before limit
     {
-        int temp = 2*i + 1;
+        int inc = 2 * i + 1; //does not include even numbers, they will not be palindromic in binary
+        int temp = inc;
         vector<int> vect;
-        
-        if (palCheck(temp, vect) == true && palCheck(decToBinary(temp), vect) == true)
+        while (temp != 0) //separates ints into digits
         {
-            cout << temp << endl;
-            cout << decToBinary(temp) << endl;
-            total = total + temp;
+            vect.push_back(temp % 10);
+            temp /= 10;
+        }
+        if ((palCheck(vect) == true) && palCheck(decToBinary(inc)) == true)
+        {
+            cout << 2*i + 1 << endl;
+            //cout << decToBinary(temp) << endl;
+            total = total + inc;
         }
     } //end for
     cout << "The total is: " << total << endl;
